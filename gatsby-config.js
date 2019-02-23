b704771b29c5a23dfee790d5988360ccd0f37951
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2019-02-23 18:11:22 +1300

    pretty near merged

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,27 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        // keep as first gatsby-source-filesystem plugin for gatsby image support
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${__dirname}/static/img`,
+          name: 'uploads',
+        },
       },
-    },
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${__dirname}/src/pages`,
+          name: 'pages',
+        },
+      },
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${__dirname}/src/images`,
+          name: 'images',
+        },
+      },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -30,10 +45,40 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    { resolve: 'gatsby-transformer-remark',
+        options: {
+            plugins: [
+                {
+                  resolve: 'gatsby-remark-relative-images',
+                  options: {
+                    name: 'uploads',
+                  },
+                },
+                {
+                  resolve: 'gatsby-remark-images',
+                  options: {
+                    // It's important to specify the maxWidth (in pixels) of
+                    // the content container as this plugin uses this as the
+                    // base for generating different widths of each image.
+                    maxWidth: 2048,
+                  },
+                },
+                {
+                  resolve: 'gatsby-remark-copy-linked-files',
+                  options: {
+                    destinationDir: 'static',
+                  }
+                }
+              ],
+        }
+
+    },
     { resolve: 'gatsby-plugin-netlify-cms',
     options: {
 
-        modulePath: '${__dirname}/src/cms/cms'
-    }},
+        modulePath: `${__dirname}/src/cms/cms`,
+    }
+},'gatsby-plugin-netlify',
+
   ],
 }
