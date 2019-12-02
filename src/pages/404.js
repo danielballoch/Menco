be687ab0eb7ff2e9ad096067bb0ed0f5d24c1182
Daniel Balloch <43333774+danielpbdesign@users.noreply.github.com>
2019-12-02 17:09:50 +1300

    update build settings

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import React from "react"
-
+import styled from '@emotion/styled';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const Center404 = styled.div`
+text-align: center;
+
+padding-top: 12vw;
+padding-bottom: 10vw;
+`
+
+
+
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Center404>
+            <SEO title="404: Not found" />
+            <h1>PAGE NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist :(</p>
+        </Center404>
   </Layout>
 )
 
